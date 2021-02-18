80f57e75f7900062d8f0614a8b50774b00d2448f
florapetersen <floramarina@comcast.net>
2021-02-17 18:16:22 -0800

    commit tree dcda07fe46a4bcd0b07c7f4fa35ac8c2888e1d5c

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleMessageChange = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  remainingChars = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          onChange={event => this.handleMessageChange(event)} 
+          value={this.state.message} 
+        />
+        {this.remainingChars()}
       </div>
     );
   }
